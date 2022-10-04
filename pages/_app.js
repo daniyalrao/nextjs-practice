98aba38c5783e5b38e828d2b010820ca1f3236b3
Rao Daniyal <daniyal@get-licensed.co.uk>
2022-10-04 18:53:25 +0500

    practice template for next js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import '../public/Gilroy/stylesheet.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
